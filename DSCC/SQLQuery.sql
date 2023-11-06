c78257e675d93244299840e315b3d52d9bc5a750
wiut-00012071 <wiut00012071@gmail.com>
2023-11-07 01:41:06 +0500

    feat: add more inserts

--- DSCC/SQLQuery.sql
+++ DSCC/SQLQuery.sql
@@ -1,40 +1,124 @@
 ﻿-- Insert data into the Departments table
 INSERT INTO Departments (Name) VALUES ('Human Resources');
+
 INSERT INTO Departments (Name) VALUES ('Information Technology');
+
 INSERT INTO Departments (Name) VALUES ('Sales');
+
 INSERT INTO Departments (Name) VALUES ('Marketing');
+
 INSERT INTO Departments (Name) VALUES ('Finance');
 
--- Insert data into the Employees table
--- Departments 1: Human Resources
+-- Departments 2: Human Resources
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (1, 'HR Manager', 'John', 'Doe', 'John Doe is the HR Manager.');
+
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (1, 'HR Specialist', 'Jane', 'Smith', 'Jane Smith is an HR Specialist.');
+
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (1, 'HR Coordinator', 'David', 'Williams', 'David Williams is an HR Coordinator.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (1, 'HR Coordinator', 'Sarah', 'Johnson', 'Sarah Johnson is an HR Coordinator.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (1, 'HR Specialist', 'Michael', 'Smith', 'Michael Smith is an HR Specialist.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (1, 'HR Assistant', 'Laura', 'Brown', 'Laura Brown is an HR Assistant.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (1, 'HR Analyst', 'Christopher', 'Wilson', 'Christopher Wilson is an HR Analyst.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (1, 'HR Coordinator', 'Rebecca', 'Jones', 'Rebecca Jones is an HR Coordinator.');
 
 -- Departments 2: Information Technology
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (2, 'Software Engineer', 'Michael', 'Johnson', 'Michael Johnson is a Software Engineer.');
+
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (2, 'Database Administrator', 'Emily', 'Davis', 'Emily Davis is a Database Administrator.');
 
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (2, 'Software Engineer', 'Jessica', 'Harris', 'Jessica Harris is a Software Engineer.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (2, 'Database Administrator', 'Daniel', 'Martinez', 'Daniel Martinez is a Database Administrator.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (2, 'IT Support Specialist', 'Megan', 'Clark', 'Megan Clark is an IT Support Specialist.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (2, 'Software Developer', 'Matthew', 'Robinson', 'Matthew Robinson is a Software Developer.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (2, 'IT Manager', 'Amanda', 'Young', 'Amanda Young is an IT Manager.');
+
 -- Departments 3: Sales
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (3, 'Sales Representative', 'Richard', 'Evans', 'Richard Evans is a Sales Representative.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (3, 'Sales Associate', 'Patricia', 'Garcia', 'Patricia Garcia is a Sales Associate.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (3, 'Sales Coordinator', 'Kevin', 'Wright', 'Kevin Wright is a Sales Coordinator.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (3, 'Sales Manager', 'Cynthia', 'Thomas', 'Cynthia Thomas is the Sales Manager.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (3, 'Sales Representative', 'Steven', 'Perez', 'Steven Perez is a Sales Representative.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (3, 'Sales Manager', 'Robert', 'Brown', 'Robert Brown is the Sales Manager.');
+
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (3, 'Sales Representative', 'Susan', 'Lee', 'Susan Lee is a Sales Representative.');
 
 -- Departments 4: Marketing
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (4, 'Marketing Coordinator', 'Rachel', 'Hall', 'Rachel Hall is a Marketing Coordinator.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (4, 'Marketing Specialist', 'Andrew', 'Lewis', 'Andrew Lewis is a Marketing Specialist.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (4, 'Marketing Assistant', 'Olivia', 'Hernandez', 'Olivia Hernandez is a Marketing Assistant.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (4, 'Marketing Manager', 'Jason', 'Gonzalez', 'Jason Gonzalez is the Marketing Manager.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (4, 'Digital Marketer', 'Natalie', 'Turner', 'Natalie Turner is a Digital Marketer.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (4, 'Marketing Director', 'Thomas', 'Moore', 'Thomas Moore is the Marketing Director.');
+
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (4, 'Marketing Coordinator', 'Karen', 'Wilson', 'Karen Wilson is a Marketing Coordinator.');
 
 -- Departments 5: Finance
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (5, 'Financial Analyst', 'James', 'Scott', 'James Scott is a Financial Analyst.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (5, 'Finance Specialist', 'Elizabeth', 'Miller', 'Elizabeth Miller is a Finance Specialist.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (5, 'Finance Assistant', 'Brian', 'Adams', 'Brian Adams is a Finance Assistant.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (5, 'Finance Manager', 'Jennifer', 'Baker', 'Jennifer Baker is the Finance Manager.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
+VALUES (5, 'Financial Planner', 'William', 'Harris', 'William Harris is a Financial Planner.');
+
+INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (5, 'Finance Manager', 'William', 'Taylor', 'William Taylor is the Finance Manager.');
+
 INSERT INTO Employees (DepartmentId, JobTitle, FirstName, LastName, Bio)
 VALUES (5, 'Financial Analyst', 'Linda', 'Anderson', 'Linda Anderson is a Financial Analyst.');
--- Add more employees as needed
+
